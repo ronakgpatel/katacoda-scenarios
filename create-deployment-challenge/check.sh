cb5b6ba79fa23859ea6a8079a8ac5dac24b373b6
ronakgpatel <ronakpatel1685@gmail.com>
2019-12-13 11:51:46 +0530

    Added challenge for deployment

---
+++ create-deployment-challenge/check.sh
@@ -0,0 +1,11 @@
+expected_namespace_name="my-ns"
+actual_namespace_name=`k get ns my-ns  -o=jsonpath='{.metadata.name}{"\n"}'`
+expected_deployment_name="nginx-deploy"
+actual_deployment_name=`k get deployments -n my-ns -o=jsonpath='{.items[0].metadata.name}{"\n"}'`
+expected_replicas=5
+acutal_replicas=`k get deployments -n my-ns -o=jsonpath='{.items[0].spec.replicas}{"\n"}'`
+
+if [ $actual_namespace_name = $expected_namespace_name -a $expected_deployment_name = $actual_deployment_name -a $expected_replicas = $acutal_replicas]
+  then
+        echo "done"
+fi                
