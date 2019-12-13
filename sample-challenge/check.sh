154e8fbda53e3c20bc726f1ba174a7b8c7a8a9d3
ronakgpatel <ronakpatel1685@gmail.com>
2019-12-13 11:19:45 +0530

    Test

--- sample-challenge/check.sh
+++ sample-challenge/check.sh
@@ -1,6 +1,7 @@
 expected_container_name="busybox-container-1"
-cont_name=`kubectl get pods busybox-pod -o=jsonpath='{.spec.containers[0].name}{"\n"}'`
-if [ $cont_name =  $expected_container_name ]
+cont_name=`kubectl get pods busybox-pod -o=jsonpath='{.status.containerStatuses[0].name}{"\n"}'`
+cont_status=`kubectl get pods busybox-pod -o=jsonpath='{.status.containerStatuses[0].ready}{"\n"}'`
+if [ $cont_name = $expected_container_name -a $cont_status = "true" ]
   then
         echo "done"
   else
