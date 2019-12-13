6a565f667efea8eb7756bf0b38963a49d335635f
RonakPatel <ronakpatel1685@gmail.com>
2019-12-13 22:14:16 +0530

    Test

--- quiz-1/assets/wait-script.sh
+++ quiz-1/assets/wait-script.sh
@@ -6,6 +6,9 @@ until [ $count -eq 1 ] ; do
     sleep 1
     count=`netstat -a  | grep 6443 | grep LISTEN | wc -l`
 done
-
+echo -e "."
 echo -e "Loading Completed..."
 
+echo -e "Creating objects..."
+
+`kubectl create -f /var/data/all.yaml`
