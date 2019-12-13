9ec6bc9502692c973a8dfa0c4db7b89e3ac1410d
ronakgpatel <ronakpatel1685@gmail.com>
2019-12-13 17:05:59 +0530

    Test'

--- quiz-1/wait-script.sh
+++ quiz-1/wait-script.sh
@@ -2,6 +2,6 @@ echo -e "Waiting....."
 count=`netstat -a  |grep 6443 | grep LISTEN | wc -l`
 until [ $count -eq 1 ] ; do
     printf '.'
-    echo $count
     sleep 1
+    count=`netstat -a  |grep 6443 | grep LISTEN | wc -l`
 done
