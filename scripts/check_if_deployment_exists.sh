66e7b3c7a73e2244e0b3b5e6845eccf9d0692862
RonakPatel <ronakpatel1685@gmail.com>
2019-12-14 15:11:54 +0530

    Added script for verification of deployment

---
+++ scripts/check_if_deployment_exists.sh
@@ -0,0 +1,2 @@
+source verify_script.sh
+verify_if_deployment_exists "nginx-deploy" "default" "2"
--- scripts/verify_script.sh
+++ scripts/verify_script.sh
@@ -7,6 +7,24 @@ check_if_equal $expected_pod_name $actual_pod_name
 
 }
 
+verify_if_deployment_exists() {
+# First argument is deployment name
+# second is namespace, if not provided would be "default"
+# third argument is expected replica count, default =1
+
+expected_ns="${2:-default}"
+expected_replica_count="${3:-1}"
+expected_deployment_name="$1"
+
+actual_deployment_name=`kubectl get deployments -n $expected_ns -o=jsonpath='{.items[0].metadata.name}{"\n"}'`
+acutal_replicas=`kubectl get deployments -n $expected_ns -o=jsonpath='{.items[0].spec.replicas}{"\n"}'`
+
+check_if_equal_and_contd $expected_deployment_name $actual_deployment_name 
+check_if_equal $expected_replica_count $acutal_replicas 
+
+}
+
+
 verify_if_pod_has_only_these_labels(){
 
 expected_labels="map[$2]"
@@ -23,6 +41,18 @@ expected="$1"
 actual=`kubectl get ns $1  -o=jsonpath='{.metadata.name}{"\n"}'`
 
 check_if_equal $expected $actual
+
+}
+
+
+check_if_equal_and_contd(){
+
+if [ ! -z $1 ] && [ ! -z $2 ] && [ $1 = $2 ] 
+then
+      printf ""
+else
+      echo "Expected : $1, Actual : $2"
+fi
 
 }
 
