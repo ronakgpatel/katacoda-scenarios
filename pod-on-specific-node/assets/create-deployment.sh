6109c0cb0f0891f2a176dfbefec3d19c492cfbd6
ronakgpatel <ronakpatel1685@gmail.com>
2019-12-19 14:40:05 +0530

    Adding taints

--- pod-on-specific-node/assets/create-deployment.sh
+++ pod-on-specific-node/assets/create-deployment.sh
@@ -1,1 +1,4 @@
+`kubectl label node node01 node-name=first`
+`kubectl taint nodes node01 dedicated=work-user:NoSchedule`
+`kubectl taint nodes master dedicated=admin-user:NoSchedule`
 `kubectl create -f /var/data/create-deployment.yaml`
